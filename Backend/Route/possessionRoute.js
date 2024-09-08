84985995acf61127d2457c6df24471cbf5eb39e0
Elitina <hei.elitina@gmail.com>
2024-09-08 22:39:25 +0300

    feat: update possessionRoute

--- Backend/Route/possessionRoute.js
+++ Backend/Route/possessionRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/', getPossessions);
 router.post('/', addPossession);
 router.put('/:libelle', updatePossession);
-router.patch('/:libelle/close', closePossession);
+router.patch('/:libelle', closePossession);
 router.get('/:libelle', getPossessionByLibelle);
 
 export default router;
